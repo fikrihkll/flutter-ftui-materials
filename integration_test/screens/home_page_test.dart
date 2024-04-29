6ebe8e08b43768df4f933c5f8a7146e4ebddd40e
zackmolt <fkrihkl@gmail.com>
2024-04-29 09:58:29 +0700

    feat: create sample page and create integration test

---
+++ integration_test/screens/home_page_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_world/screens/home_page.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Input number 2 and 6 then press Add Number Button should show 4 and 10', (WidgetTester tester) async {
+    // Create HomePage
+    await tester.pumpWidget(
+        MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(),
+          home: const HomePage(),
+        )
+    );
+
+    // Find TextField with the key "textfield_number" and Type number 2
+    await tester.enterText(
+        find.byKey(const Key("textfield_number")),
+        "2"
+    );
+
+    // Find any widget text Add Number then tap the widget
+    await tester.tap(find.text("Add Number"));
+    await tester.tap(find.text("Add Number"));
+    await tester.pump();
+
+    // Query one widget with the key "text_added_number" then store it in a variable
+    Text text = tester.firstWidget(find.byKey(const Key("text_added_number")));
+    // Expect the text data is 4
+    expect(text.data, "4");
+
+    // Find TextField with the key "textfield_number" and Type number 6
+    await tester.enterText(
+        find.byKey(const Key("textfield_number")),
+        "6"
+    );
+
+    // Find any widget text Add Number then tap the widget
+    await tester.tap(find.text("Add Number"));
+    await tester.pump();
+
+    // Query one widget with the key "text_added_number" then store it in a variable
+    text = tester.firstWidget(find.byKey(const Key("text_added_number")));
+    // Expect the text data is 10
+    expect(text.data, "10");
+  });
+
+
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,14 +8,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final TextEditingController _numberController = TextEditingController();
+  int _addedNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text("Hi!"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                key: const Key("textfield_number"),
+                controller: _numberController,
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              MaterialButton(
+                color: Colors.blue,
+                onPressed: () {
+                  _addNumber();
+                },
+                child: const Text("Add Number")
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                  _addedNumber.toString(),
+                key: const Key("text_added_number"),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
+  void _addNumber() {
+    try {
+      int newNumber = int.parse(_numberController.text);
+      _addedNumber += newNumber;
+      setState(() {});
+    } on FormatException catch (e) {}
+  }
+
 }
