bbe04e9cca38350ab9fab334649c2b03e1414313
Fikri Haikal <fkrihkl@gmail.com>
2024-06-10 10:38:59 +0700

    feat: create remote helper

--- lib/utils/remote_helper.dart
+++ lib/utils/remote_helper.dart
@@ -4,7 +4,17 @@ class RemoteHelper {
   static Dio? _dio;
 
   static Dio getDio() {
-    return _dio ??= Dio(BaseOptions(baseUrl: "https://jsonplaceholder.typicode.com/"));
+    return _dio ??= Dio(BaseOptions(baseUrl: "https://dummyjson.com/"));
+  }
+
+  static setToken(String token) {
+    _dio?.options = BaseOptions(
+      baseUrl: "https://dummyjson.com/",
+      headers: {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json"
+      }
+    );
   }
 
 }
