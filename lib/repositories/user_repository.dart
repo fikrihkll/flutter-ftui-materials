bd76ef0057ee190e782960462852635e097c304e
Fikri Haikal <fkrihkl@gmail.com>
2024-06-10 10:36:29 +0700

    feat: create user repository

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,30 +1,79 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hello_world/models/remote/api_result.dart';
+import 'package:hello_world/models/remote/user_response.dart';
 import 'package:hello_world/utils/remote_helper.dart';
+import 'package:hello_world/utils/shared_preference_helper.dart';
 
 class UserRepository {
 
-  Future<Response?> getAllUsers() async {
+  final Dio dio;
+  final FlutterSecureStorage sharedPreference;
+  UserRepository(this.dio, this.sharedPreference);
+
+  Future<ApiResult<UserResponse>> login(String email, String password) async {
     try {
-      var data = await RemoteHelper.getDio()
-          .get("users");
-      return data;
+      var requestBody = {
+        "username": email,
+        "password": password
+      };
+      var jsonResult = await dio
+          .post("auth/login", data: requestBody);
+      var userData = UserResponse.fromJson(jsonResult.data);
+      await _saveLoginData(userData.token);
+      RemoteHelper.setToken(userData.token);
+
+      return ApiResult(data: userData);
     } on DioException catch(e) {
-      return e.response;
+      try {
+        return ApiResult(
+            isSuccess: false,
+            message: (e.response?.data as Map<String, dynamic>)["message"] ?? ""
+        );
+      } catch(e) {
+        return ApiResult(isSuccess: false, message: e.toString());
+      }
     } on Exception catch(e) {
-      return null;
+      debugPrint("Er ${e}");
+      return ApiResult(isSuccess: false, message: e.toString());
     }
   }
 
-  Future<Response?> getUserById(String userId) async {
+  Future<ApiResult<UserResponse>> getUserProfile() async {
     try {
-      var data = await RemoteHelper.getDio()
-          .get("users/$userId");
-      return data;
+      var jsonResult = await dio.get("auth/me");
+
+      var userData = UserResponse.fromJson(jsonResult.data);
+      return ApiResult(data: userData);
     } on DioException catch(e) {
-      return e.response;
+      return ApiResult(
+          isSuccess: false,
+          message: (e.response?.data as Map<String, dynamic>)["message"] ?? ""
+      );
     } on Exception catch(e) {
-      return null;
+      return ApiResult(isSuccess: false, message: e.toString());
     }
+  }
+
+  Future<void> clearToken() async {
+    await sharedPreference
+        .delete(key: SharedPreferenceHelper.KEY_TOKEN);
+  }
+
+  Future<bool> tokenExist() async {
+    String? token = await sharedPreference.read(key: SharedPreferenceHelper.KEY_TOKEN);
+    if (token != null) {
+      RemoteHelper.setToken(token);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> _saveLoginData(String token) async {
+    await sharedPreference
+        .write(key: SharedPreferenceHelper.KEY_TOKEN, value: token);
   }
 
 }
